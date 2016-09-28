ddc117ec68d688c9d48a695d2799f779ee71b645
Marco Lancini <info@marcolancini.it>
2016-09-28 12:28:17 +0100

    Added support for SSH pub key auth

--- needle/core/framework/cli.py
+++ needle/core/framework/cli.py
@@ -51,13 +51,13 @@ class CLI(Framework):
         self.register_option('port', Constants.GLOBAL_PORT, True, 'Port of the SSH agent on the testing device (needs to be != 22 to use USB)')
         self.register_option('username', Constants.GLOBAL_USERNAME, True, 'SSH Username of the testing device')
         self.register_option('password', Constants.GLOBAL_PASSWORD, True, 'SSH Password of the testing device')
-        self.register_option('proxy', None, False, 'Proxy server (address:port)')
+        self.register_option('pub_key_auth', Constants.GLOBAL_PUB_KEY_AUTH, True, 'Use public key auth to authenticate to the device. Key must be present in the ssh-agent if a passphrase is used')
         self.register_option('debug', Constants.GLOBAL_DEBUG, True, 'Enable debugging output')
         self.register_option('verbose', Constants.GLOBAL_VERBOSE, True, 'Enable verbose output')
         self.register_option('app', '', False, 'Bundle ID of the target application (e.g., com.example.app). Leave empty to launch wizard')
         self.register_option('setup_device', Constants.GLOBAL_SETUP_DEVICE, True, 'Set to true to enable auto-configuration of the device (installation of all the tools needed)')
         self.register_option('output_folder', Constants.GLOBAL_OUTPUT_FOLDER, True, 'Full path of the output folder, where to store the output of the modules')
-        self.register_option('pub_key_auth', Constants.GLOBAL_PUB_KEY_AUTH, True, 'Use public key auth. Key must be present in the ssh-agent if a passphrase is used')
+        #self.register_option('proxy', None, False, 'Proxy server (address:port)')
 
     def _init_global_vars(self):
         # Setup Printer
