3ff893d4a8406de3c6fecf7a8fede421b1f02678
Oliver Simonnet <oliversimonnet@gmail.com>
2016-09-30 00:26:39 +0100

    Optimized locating of configuration files

--- needle/modules/storage/data/files_mdm.py
+++ needle/modules/storage/data/files_mdm.py
@@ -9,9 +9,10 @@ class Module(BaseModule):
         'author': 'Oliver Simonnet (@MWRLabs)',
         'description': 'Locate and dump MDM configuration files',
         'options': (
-            ('dump_all', False, False, 'Dump all configuration files in a selected directory'),
-            ('autosave', False, False, 'Automatically save files. (No save prompt)'),
-            ('output', True, True, 'Full path of the output folder')
+            ('dump_all', False, False, 'DUMP_ALL mode. Dump all configuration files in a selected directory'),
+            ('autosave', False, False, 'Automatically save files. (Recomended with DUMP_ALL mode)'),
+            ('silent', True, True, 'Silent mode. Will not print file contents to screen.'),
+            ('output', True, True, 'Ful,l path of the output folder')
         ),
     }
 
@@ -32,14 +33,26 @@ class Module(BaseModule):
 
     # Compose cmd string, and return cmd output
     def get_files(self, dir_str):
-        cmd = '{bin} {dirs_str} -type f -name "*.plist"'.format(bin=self.device.DEVICE_TOOLS['FIND'], dirs_str=dir_str)
+        cmd = '{bin} {dirs_str} -maxdepth 1 -type f -name "*.plist"'.format(bin=self.device.DEVICE_TOOLS['FIND'], dirs_str=dir_str)
         out = self.device.remote_op.command_blocking(cmd)
         return out
 
     # Filter out duplicate directories, and return list
     def filter_dirs(self, dirs):
-        directories = list(set([Utils.extract_directory_from_path(x) for x in dirs]))
+        directories = list(set([Utils.extract_directory_from_path(x) for x in dirs if "needle" not in x]))
         return directories
+
+    def set_output_name(self, remote_file):
+        fileName = Utils.extract_filename_from_path(remote_file)
+        fileName = 'mdm_export_{}'.format(fileName)
+        return self.local_op.build_output_path_for_file(self, fileName)
+
+    def save_file(self, remote_file, local_file):
+        if self.options['autosave']:
+            self.device.pull(remote_file, local_file)
+        else:
+            save = choose_yes_no("Would you like to pull ths file?")
+            if save: self.device.pull(remote_file, local_file)
 
      # Run standard mode
     def individual_mode(self, directories):
@@ -55,44 +68,30 @@ class Module(BaseModule):
             pl = dict(self.device.remote_op.parse_plist(option))
 
             # Initialize output filename
-            if self.options['output']:
-                fileName = Utils.extract_filename_from_path(option.strip())
-                fileName = 'mdm_export_{}'.format(fileName)
-                outfile = self.local_op.build_output_path_for_file(self, fileName)
-            else: 
-                outfile = None
+            outFile = self.set_output_name(option.strip())
 
-            # Print Data to user
-            self.print_cmd_output(pl)
-
-            if self.options['autosave']:
-                self.device.pull(option.strip(), outfile)
-            else:
-                save = choose_yes_no("Would you like to pull ths file?")
-                if save: self.device.pull(option.strip(), outfile)
+            # Print Data to user and save file
+            if not self.options['silent']: self.print_cmd_output(pl)
+            self.save_file(option.strip(), outFile)
                     
             # Ask user if they wish to view another file
             yes = choose_yes_no("Would you like to inspect another file?")
             if not yes: self.running = False
 
-    # Run dump_all gather mode
-    def dump_all_mode(self, directories):
+    # Run dump mode
+    def dump_mode(self, directories):
         while self.running:
             dirs_str = self.select_dir(directories)
             retrieved_files = self.get_files(dirs_str)
 
             for i in range(len(retrieved_files)):
                 f = retrieved_files[i].strip()
-                fileName = Utils.extract_filename_from_path(f)
-                fileName = 'mdm_export_{}'.format(fileName)
-                outfile = self.local_op.build_output_path_for_file(self, fileName)
-                
-                if self.options['autosave']:
-                    self.device.pull(f, outfile)
-                else:
-                    self.printer.info("File: %s (%d/%d)" % (f, i+1, len(retrieved_files)))
-                    save = choose_yes_no("Would you like to pull ths file?")
-                    if save: self.device.pull(f, outfile)
+                # Initialize output
+                outFile = self.set_output_name(f)
+
+                # filename and save file
+                self.printer.info("File: %s (%d/%d)" % (f, i+1, len(retrieved_files)))
+                self.save_file(f, outFile)
 
             # Ask the user if they want to dump another directory
             yes = choose_yes_no("Would you like to dump another directory?")
@@ -104,6 +103,9 @@ class Module(BaseModule):
     def module_run(self):
         self.running = True
         self.printer.info("Looking for MDM Configuration file locations...")
+
+        if self.options['dump_all'] and not self.options['autosave']:
+            self.printer.warning("Autosave recomended in DUMO_ALL mode!")
 
         # Find MDM config file locations
         cmd = '{bin} / -type f -name "MDM*.plist" -o -name "UserSettings.plist"'.format(bin=self.device.DEVICE_TOOLS['FIND'])
@@ -118,5 +120,5 @@ class Module(BaseModule):
         if not self.options['dump_all']:
             self.individual_mode(directories)
         else:
-            self.printer.info("Running in dump_all mode!")
-            self.dump_all_mode(directories)
+            self.printer.info("Running in dump mode!")
+            self.dump_mode(directories)
