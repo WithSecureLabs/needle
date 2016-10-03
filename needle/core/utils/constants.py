f46a7d21888c2845b2a29c2679c68ff0379f8396
Marco Lancini <info@marcolancini.it>
2016-10-03 18:30:30 +0100

    Set SETUP_DEVICE to False as default

--- needle/core/utils/constants.py
+++ needle/core/utils/constants.py
@@ -27,7 +27,7 @@ class Constants(object):
     GLOBAL_PASSWORD = 'alpine'
     GLOBAL_DEBUG = False
     GLOBAL_VERBOSE = True
-    GLOBAL_SETUP_DEVICE = True
+    GLOBAL_SETUP_DEVICE = False
     GLOBAL_OUTPUT_FOLDER = os.path.join(FOLDER_HOME, 'output')
     GLOBAL_PUB_KEY_AUTH = True
 
