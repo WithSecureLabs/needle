37664bd9e272fbde3956a3c363e24dc06d644f3b
tghosth <janda26@hotmail.com>
2016-10-16 12:47:47 +0300

    Improve error handling and add link to note about OpenSSL

--- needle/modules/comms/certs/view_cert.py
+++ needle/modules/comms/certs/view_cert.py
@@ -10,7 +10,8 @@ class Module(BaseModule):
             ('url', 'google.com', True, 'Site URL to check (https:// will be added to the front'),
             ('proxy', None, False, 'HTTP proxy to use when checking in the form host:port')
         ),
-        'comments': ['This script doesn\'t currently verify the validity of the certificate.']
+        'comments': ['This script doesn\'t currently verify the validity of the certificate.',
+                     'https://github.com/mwrlabs/needle/pull/62#issuecomment-254037231']
     }
 
     # ==================================================================================================================
@@ -29,15 +30,23 @@ class Module(BaseModule):
     def module_run(self):
 
         # Start building the cURL command
-        cmd = '{curl} --insecure -v https://{url} '.format(curl=self.device.DEVICE_TOOLS['CURL'], url=self.options['url'])
+        cmd = '{curl} --insecure https://{url} '.format(curl=self.device.DEVICE_TOOLS['CURL'], url=self.options['url'])
 
         # Add a proxy if relevant
         if self.options['proxy'] is not None:
             cmd += ' -x {} '.format(self.options['proxy'])
 
+        # Run the command for the first time to check for errors
+        # This command will automatically report if the URL is incorrect or if there is an issue with the proxy
+        self.device.remote_op.command_blocking('{} --fail --silent --show-error'.format(cmd))
+
+        self.printer.info('Getting the certificate...')
+
         # Use awk to display only the relevant lines from the output
-        cmd += " 2>&1 | awk 'BEGIN { cert=0 } /^\* Server certificate:/ { cert=1 } /^\*/ { if (cert) print }'"
+        cmd += "-v 2>&1 | awk 'BEGIN { cert=0 } /^\* Server certificate:/ { cert=1 } /^\*/ { if (cert) print }'"
 
         # Run the command and print to screen
+        # This command will automatically report if the URL is incorrect or if there is an issue with the proxy
         out = self.device.remote_op.command_blocking(cmd)
+
         self.print_cmd_output(out)
