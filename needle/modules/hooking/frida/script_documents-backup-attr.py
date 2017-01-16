2e75021fb214d250d681a3a84c0cc8d42215dec4
Marco Lancini <info@marcolancini.it>
2017-01-16 09:28:25 +0000

    [FEATURE] NSURLIsExcludedFromBackupKey module

--- needle/modules/hooking/frida/script_documents-backup-attr.py
+++ needle/modules/hooking/frida/script_documents-backup-attr.py
@@ -11,33 +11,33 @@ class Module(FridaScript):
         ),
     }
 
-    JS = '''
-        if (ObjC.available) {
-          var NSHomeDirectory = new NativeFunction(ptr(Module.findExportByName("Foundation","NSHomeDirectory")),'pointer',[]);
-          var NSFileManager = ObjC.classes.NSFileManager;
-          var NSURL = ObjC.classes.NSURL;
+    JS = '''\
+if (ObjC.available) {
+    var NSHomeDirectory = new NativeFunction(ptr(Module.findExportByName("Foundation","NSHomeDirectory")),'pointer',[]);
+    var NSFileManager = ObjC.classes.NSFileManager;
+    var NSURL = ObjC.classes.NSURL;
 
-          var documentsPath = (new ObjC.Object(NSHomeDirectory())).stringByAppendingPathComponent_("Documents");
-          var enumerator = NSFileManager.defaultManager().enumeratorAtPath_(documentsPath);
+    var documentsPath = (new ObjC.Object(NSHomeDirectory())).stringByAppendingPathComponent_("Documents");
+    var enumerator = NSFileManager.defaultManager().enumeratorAtPath_(documentsPath);
+    var filePath = null;
 
-          var filePath = null;
+    var isDirPtr = Memory.alloc(Process.pointerSize);
+    Memory.writePointer(isDirPtr,NULL);
 
-          var isDirPtr = Memory.alloc(Process.pointerSize);
-          Memory.writePointer(isDirPtr,NULL);
-
-          while ((filePath = enumerator.nextObject()) != null){
-            NSFileManager.defaultManager().fileExistsAtPath_isDirectory_(documentsPath.stringByAppendingPathComponent_(filePath),isDirPtr);
-            var url = NSURL.fileURLWithPath_(documentsPath.stringByAppendingPathComponent_(filePath));
-            if (Memory.readPointer(isDirPtr) == 0){
-              var resultPtr = Memory.alloc(Process.pointerSize);
-              var errorPtr = Memory.alloc(Process.pointerSize);
-              url.getResourceValue_forKey_error_(resultPtr,"NSURLIsExcludedFromBackupKey",errorPtr)
-              var result = new ObjC.Object(Memory.readPointer(resultPtr));
-              send(JSON.stringify({result:result.toString(), path:documentsPath.stringByAppendingPathComponent_(filePath).toString()}));
-
-            }
-          }
+    while ((filePath = enumerator.nextObject()) != null){
+        NSFileManager.defaultManager().fileExistsAtPath_isDirectory_(documentsPath.stringByAppendingPathComponent_(filePath),isDirPtr);
+        var url = NSURL.fileURLWithPath_(documentsPath.stringByAppendingPathComponent_(filePath));
+        if (Memory.readPointer(isDirPtr) == 0) {
+            var resultPtr = Memory.alloc(Process.pointerSize);
+            var errorPtr = Memory.alloc(Process.pointerSize);
+            url.getResourceValue_forKey_error_(resultPtr,"NSURLIsExcludedFromBackupKey",errorPtr)
+            var result = new ObjC.Object(Memory.readPointer(resultPtr));
+            send(JSON.stringify({result:result.toString(), path:documentsPath.stringByAppendingPathComponent_(filePath).toString()}));
         }
+    }
+} else {
+    console.log("Objective-C Runtime is not available!");
+}
         '''
 
     # ==================================================================================================================
