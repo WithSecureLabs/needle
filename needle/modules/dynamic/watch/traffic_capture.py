791f3e04625f19d2a324ac548bce7732bbb51d74
Andrea Amendola <andrea.amendola@mwrinfosecurity.com>
2017-03-29 16:57:56 +0100

    Activate firewall rules on the device

--- needle/modules/dynamic/watch/traffic_capture.py
+++ needle/modules/dynamic/watch/traffic_capture.py
@@ -7,9 +7,7 @@ class Module(BackgroundModule):
         'author': '@Andrea Amendola',
         'description': 'Description',
         'options': (
-            ('ip', '192.168.1.133', True, 'ip of the machine that will capture the traffic'),
-            ('port', '7575', True, 'port of the service that will handle the captured traffic'),
-            ('program', 'WIRESHARK', True, 'Select the program to use for viewing the captured traffic. Currently supported: WIRESHARK'),
+            ('port', '7575', True, 'port of the service that will handle the captured traffic'),            
         ),
     }
 
@@ -24,19 +22,36 @@ class Module(BackgroundModule):
     # ==================================================================================================================
     def module_run(self):
         """Main Execution"""
-        # Ensure a valid net traffic viewer has been specified
-        self.validate_net_traffic_viewer()
+        
+        # Uploading firewall rules
+        self.printer.info('Activating firewall rules...')                 
+        local_temp_file = self.local_op.build_temp_path_for_file("needle-pfctl.rules", self)
+        remote_temp_file = '/etc/needle-pfctl.rules'
 
-        # Launch net traffic viewer
-        self.printer.info('Launching ' + self.net_traffic_viewer +  '...')
-        cmd = '{bin} -lnp 7575 | {viewer} -k -i -'.format(bin=self.TOOLS_LOCAL['NC'], viewer=self.net_traffic_viewer)
-        self.local_op.command_subproc_start(cmd, piping=True)
+        self.local_op.write_file(local_temp_file, 'rdr on lo0 inet proto tcp from any to any port 80 -> 127.0.0.1 port 9999\n'
+                                                  'pass out route-to (lo0 127.0.0.1) inet proto tcp from any to any port 80\n')
+        # Disabling current firewall rules
+        #self.device.remote_op.command_blocking('pfctl -d', internal=False)
+        self.device.remote_op.upload(local_temp_file, remote_temp_file, recursive=False)
+
+        self.device.remote_op.command_blocking('pfctl -e -f /etc/needle-pfctl.rules', internal=False)
+        self.printer.notify('Firewall rules activated.')
+
+        # Running remote port forwarding
+
+        #self.local_op.cat_file(temp_path)
+
+
+
+        #self.local_op.command_blocking(cmd,
         
         # Start capture
-        self.printer.info('Starting traffic capture...')
-        cmd = "{bin1} -nn -w - -U -s 0 | {bin2} {ip} {port}".format(bin1=self.device.DEVICE_TOOLS['TCPDUMP'], bin2=self.device.DEVICE_TOOLS['NC'], ip=self.options['ip'], port=self.options['port'])
-        self.device.remote_op.command_background_start(self, cmd, stream=True)        
+        #self.printer.info('Starting traffic capture...')
+        #cmd = "{bin1} -nn -w - -U -s 0 | {bin2} {ip} {port}".format(bin1=self.device.DEVICE_TOOLS['TCPDUMP'], bin2=self.device.DEVICE_TOOLS['NC'], ip=self.options['ip'], port=self.options['port'])
+        #self.device.remote_op.command_background_start(self, cmd, stream=True)        
+        return
 
     def module_kill(self):
-        self.device.remote_op.kill_proc('tcpdump')
-        self.printer.info('Traffic capture stopped.')
+        #self.device.remote_op.kill_proc('tcpdump')
+        #self.printer.info('Traffic capture stopped.')
+        return
