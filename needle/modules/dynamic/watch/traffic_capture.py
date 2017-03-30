9b05b70e3da1e31a720c47b4454e13f84700e444
Andrea Amendola <andrea.amendola@mwrinfosecurity.com>
2017-03-30 17:37:29 +0100

    Forward as thread (need to go as process)

--- needle/modules/dynamic/watch/traffic_capture.py
+++ needle/modules/dynamic/watch/traffic_capture.py
@@ -48,11 +48,10 @@ class Module(BackgroundModule):
         try:
             sock.connect((host, port))
         except Exception as e:
-            self.printer.info('Forwarding request to %s:%d failed: %r' % (host, port, e))
+            #self.printer.info('Forwarding request to %s:%d failed: %r' % (host, port, e))
             return   
     
-        self.printer.info('Connected!  Tunnel open %r -> %r -> %r' % (chan.origin_addr,
-                                                            chan.getpeername(), (host, port)))
+        #self.printer.info('Connected!  Tunnel open %r -> %r -> %r' % (chan.origin_addr, chan.getpeername(), (host, port)))
         while True:
             r, w, x = select.select([sock, chan], [], [])
             if sock in r:
@@ -67,7 +66,7 @@ class Module(BackgroundModule):
                 sock.send(data)
         chan.close()
         sock.close()
-        self.printer.info('Tunnel closed from %r' % (chan.origin_addr,))     
+        #self.printer.info('Tunnel closed from %r' % (chan.origin_addr,))     
 
     def _reverse_forward_tunnel(self, server_port, remote_host, remote_port, transport):
         transport.request_port_forward('', server_port)
@@ -75,29 +74,35 @@ class Module(BackgroundModule):
             chan = transport.accept(1000)
             if chan is None:
                 continue
-            thr = threading.Thread(target=self._handler, args=(chan, remote_host, remote_port))
-            thr.setDaemon(True)
-            thr.start()
+            #thr = threading.Thread(target=self._handler, args=(chan, remote_host, remote_port))
+            #thr.setDaemon(True)
+            #thr.start()
+            self._handler(chan, remote_host, remote_port)
 
-    def forward_to_proxy(self):
-        g_verbose = True
+    def _forward_to_proxy_intermediate(self):       
+
         client = paramiko.SSHClient()
         client.load_system_host_keys()
         client.set_missing_host_key_policy(paramiko.WarningPolicy())
 
-        self.printer.info('Connecting to ssh host %s:%d ...' % (self.device._ip, self.device._port))
+        #self.printer.info('Connecting to ssh host %s:%d ...' % (self.device._ip, self.device._port))
         try:
             client.connect(self.device._ip, self.device._port, username=self.device._username, password=self.device._password)
         except Exception as e:
             print('*** Failed to connect to %s:%d: %r' % (sself.device._ip, self.device._port, e))
             sys.exit(1)
 
-        self.printer.info('Now forwarding remote port %d to %s:%d ...' % (9999, '127.0.0.1', 8080))
+        #self.printer.info('Now forwarding remote port %d to %s:%d ...' % (9999, '127.0.0.1', 8080))
 
         try:
-            self._reverse_forward_tunnel(9999, '127.0.0.1', 8080, client.get_transport())
+            self._reverse_forward_tunnel(9999, '127.0.0.1', int(self.options['port']), client.get_transport())
         except KeyboardInterrupt:
             print('C-c: Port forwarding stopped.')
+
+    def _forward_to_proxy(self):
+        self.tunnel = threading.Thread(target=self._forward_to_proxy_intermediate)
+        self.tunnel.setDaemon(False)
+        self.tunnel.start()
 
     # ==================================================================================================================
     # RUN
@@ -121,7 +126,7 @@ class Module(BackgroundModule):
 
         # Running remote port forwarding
         self.printer.info('Activating port forwarding...')
-        self.forward_to_proxy()
+        self._forward_to_proxy()
         self.printer.notify('Portforwarding activated.')
         
         #cmd = "sshpass -p {ssh_pass} ssh -R 9999:127.0.0.1:8080 root@{device_ip}".format(ssh_pass=self.device._password, device_ip=self.device._ip)
@@ -147,5 +152,10 @@ class Module(BackgroundModule):
         #self.printer.notify('Portforwarding deactivated.')
 
         #self.command_background_stop('sshpass')
+
+        # Disabbling remote forwarding
+        self.printer.info('Deactivating port forwarding...')
+        self.tunnel._stop()
+        self.printer.notify('Portforwarding deactivated.')
 
         return
