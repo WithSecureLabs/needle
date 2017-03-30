870c71a8c58e25da45938903b551c598f6201bc3
Andrea Amendola <andrea.amendola@mwrinfosecurity.com>
2017-03-30 14:33:34 +0100

    Add forwarding

--- needle/modules/dynamic/watch/traffic_capture.py
+++ needle/modules/dynamic/watch/traffic_capture.py
@@ -8,7 +8,8 @@ class Module(BackgroundModule):
         'author': '@Andrea Amendola',
         'description': 'Description',
         'options': (
-            ('port', '7575', True, 'port of the service that will handle the captured traffic'),            
+            ('port', '8080', True,
+             'port of the service that will handle the captured traffic'),
         ),
     }
 
@@ -22,11 +23,11 @@ class Module(BackgroundModule):
         """Setup port forward to enable communication with the proxy server running on the workstation"""
         localhost = '127.0.0.1'
         self._proxy_server = SSHTunnelForwarder(
-            (self._ip, int(self._port)),
+            self.device._ip,
             ssh_username=self.device._username,
             ssh_password=self.device._password,
-            local_bind_address=(localhost, self.options['port']),
-            remote_bind_address=(self.device._ip, self.options['port']),
+            local_bind_address=(localhost, 8080),
+            remote_bind_address=(localhost, 9999),
         )
         self._proxy_server.start()
 
@@ -40,37 +41,48 @@ class Module(BackgroundModule):
     # ==================================================================================================================
     def module_run(self):
         """Main Execution"""
-        
-        # Uploading firewall rules
-        self.printer.info('Activating firewall rules...')                 
-        local_temp_file = self.local_op.build_temp_path_for_file("needle-pfctl.rules", self)
-        remote_temp_file = '/etc/needle-pfctl.rules'
 
-        self.local_op.write_file(local_temp_file, 'rdr on lo0 inet proto tcp from any to any port 80 -> 127.0.0.1 port 9999\n'
-                                                  'pass out route-to (lo0 127.0.0.1) inet proto tcp from any to any port 80\n')
-        # Disabling current firewall rules
-        #self.device.remote_op.command_blocking('pfctl -d', internal=False)
-        self.device.remote_op.upload(local_temp_file, remote_temp_file, recursive=False)
+        # Uploading firewall rules
+        self.printer.info('Activating firewall rules...')
+        self.local_temp_file = self.local_op.build_temp_path_for_file("needle-pfctl.rules", self)
+        self.remote_temp_file = '/etc/needle-pfctl.rules'
+
+        self.local_op.write_file(self.local_temp_file, 'rdr on lo0 inet proto tcp from any to any port 80 -> 127.0.0.1 port 9999\n'
+                                 'pass out route-to (lo0 127.0.0.1) inet proto tcp from any to any port 80\n')
+        
+        self.device.remote_op.upload(
+            self.local_temp_file, self.remote_temp_file, recursive=False)
 
         self.device.remote_op.command_blocking('pfctl -e -f /etc/needle-pfctl.rules', internal=False)
         self.printer.notify('Firewall rules activated.')
 
         # Running remote port forwarding
-        self._portforward_proxy_start()
-
-        #self.local_op.cat_file(temp_path)
-
-
-
-        #self.local_op.command_blocking(cmd,
+        # self.printer.info('Activating port forwarding...')
+        # self._portforward_proxy_start()
+        # self.printer.notify('Portforwarding activated.')
         
-        # Start capture
-        #self.printer.info('Starting traffic capture...')
-        #cmd = "{bin1} -nn -w - -U -s 0 | {bin2} {ip} {port}".format(bin1=self.device.DEVICE_TOOLS['TCPDUMP'], bin2=self.device.DEVICE_TOOLS['NC'], ip=self.options['ip'], port=self.options['port'])
-        #self.device.remote_op.command_background_start(self, cmd, stream=True)        
+        cmd = "sshpass -p {ssh_pass} ssh -R 9999:127.0.0.1:8080 root@{device_ip} &".format(ssh_pass=self.device._password, device_ip=self.device._ip)
+        self.tunnel = self.local_op.command_background_start(cmd)
+
         return
 
     def module_kill(self):
-        #self.device.remote_op.kill_proc('tcpdump')
-        #self.printer.info('Traffic capture stopped.')
+        # Deleting local files
+        self.printer.info('Deactivating firewall rules...')        
+        self.local_op.delete_temp_file(self.local_temp_file, self)
+        
+        # Deleting remote files
+        self.device.remote_op.file_delete(self.remote_temp_file)
+
+        # Deactivating firewall rules
+        self.device.remote_op.command_blocking('pfctl -d', internal=False)
+        self.printer.notify('Firewall rules deactivated.')
+
+        # Stopping remote port forwarding
+        # self.printer.info('Deactivating port forwarding...')
+        # self._portforward_proxy_stop()
+        # self.printer.notify('Portforwarding deactivated.')
+
+        self.command_background_stop('sshpass')
+
         return
