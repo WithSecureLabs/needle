29e7f861b4f8f91dbf49e58f19835ca3e0ebe15e
Marco Lancini <info@marcolancini.it>
2017-05-04 10:03:16 +0100

    [CORE] Add ISSUE_MANAGER

--- needle/core/framework/cli.py
+++ needle/core/framework/cli.py
@@ -7,7 +7,7 @@ import re
 from ..utils.printer import Colors, Printer
 from ..utils.constants import Constants
 from framework import Framework, FrameworkException
-from issues import IssueList
+from issues import IssueManager
 from local_operations import LocalOperations
 
 # Versioning
@@ -71,7 +71,7 @@ class CLI(Framework):
         self.local_op = Framework.local_op = LocalOperations()
         self.device = Framework.device = None
         self.APP_METADATA = Framework.APP_METADATA = None
-        self.ISSUE_LIST = Framework.ISSUE_LIST = IssueList(self)
+        self.ISSUE_LIST = Framework.ISSUE_MANAGER = IssueManager(self)
 
     def _init_home(self):
         # Folders to initialize
