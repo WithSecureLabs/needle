67d5329a6fa9a061c00de35748f9a45bcdb2d091
Ken Gannon <yogehi@gmail.com>
2018-02-22 14:44:45 -0500

    Update heap_dump.py

--- needle/modules/dynamic/memory/heap_dump.py
+++ needle/modules/dynamic/memory/heap_dump.py
@@ -31,7 +31,9 @@ class Module(BaseModule):
         # Launch the app
         self.printer.info("Launching the app...")
         self.device.app.open(self.APP_METADATA['bundle_id'])
-        pid = self.device.app.search_pid(self.APP_METADATA['binary_name'])
+        binaryPath = self.APP_METADATA['binary_path'].replace("/private","")
+        binaryPath = binaryPath.replace("'","")
+        pid = self.device.app.search_pid(binaryPath)
 
         # Create temp files/folders
         dir_dumps = self.device.remote_op.build_temp_path_for_file("gdb_dumps")
